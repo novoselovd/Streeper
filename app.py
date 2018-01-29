0f7d739965615ca800a00f612e4c7de915dab488
Novoselov Dmitry <novoselovdmitrii@gmail.com>
2018-01-29 15:42:31 +0300

    qwet

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template, url_for, redirect, flash
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask_sqlalchemy import SQLAlchemy
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SelectField, validators
+from flask_wtf import FlaskForm, Form
+from wtforms import StringField, PasswordField, BooleanField, SelectField, validators, IntegerField, SelectMultipleField
 from wtforms.validators import InputRequired, Email, Length
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -12,8 +12,10 @@ app = Flask(__name__)
 Bootstrap(app)
 
 db_path = os.path.join(os.path.dirname(__file__), 'users.db')
+channels_path = os.path.join(os.path.dirname(__file__), 'channels.db')
 db_uri = 'sqlite:///{}'.format(db_path)
 app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+app.config['SQLALCHEMY_BINDS'] = {'channels': 'sqlite:///{}'.format(channels_path)}
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SECRET_KEY'] = '2fHGGFdePK'
 
@@ -29,6 +31,16 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(80))
     type = db.Column(db.String(30))
+
+
+class Channel(db.Model):
+    __bind_key__ = 'channels'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    description = db.Column(db.String(200))
+    subscribers = db.Column(db.Integer)
+    price = db.Column(db.Integer)
+    category = db.Column(db.String(50))
 
 
 @login_manager.user_loader
@@ -50,8 +62,30 @@ class RegisterForm(FlaskForm):
         validators.EqualTo('confirm', message='Passwords do not match.')
     ])
     confirm = PasswordField('Confirm Password')
-    type = SelectField('Account type', choices=[('Brand/Agency', 'Brand/Agency'), ('Creator/Influencer', 'Creator/Influencer')])
+    type = SelectField('Account type',
+                       choices=[('Brand/Agency', 'Brand/Agency'), ('Creator/Influencer', 'Creator/Influencer')])
     tos = BooleanField('I agree to <a href="/tos">Terms of Service</a>', validators=[validators.DataRequired()])
+
+
+class CreateChannelForm(FlaskForm):
+    name = StringField('Channel', [InputRequired(), Length(min=1, max=50)])
+    category_choices = [('cars', 'cars'), ('business', 'business'),
+                        ('realty', 'realty'), ('medicine and health', 'medicine and health'),
+                        ('marketing', 'marketing'), ('work', 'work'),
+                        ('travelling', 'travelling'), ('for women', 'for women'),
+                        ('sport', 'sport'), ('culture', 'culture'),
+                        ('education', 'education'), ('products and services', 'products and services'),
+                        ('18+', '18+'), ('design and decor', 'design and decor'),
+                        ('games', 'games'), ('entertainment', 'entertainment'),
+                        ('media', 'media'), ('science and technology', 'science and technology'),
+                        ('culinary', 'culinary'), ('foreign languages', 'foreign languages'),
+                        ('motivation and self-education', 'motivation and self-education'),
+                        ('music', 'music'), ('cinematography', 'cinematography'),
+                        ('top', 'top')]
+    category = SelectField('Keys', choices=category_choices)
+    description = StringField('Channel description', [Length(max=200)])
+    subscribers = IntegerField('Number of subscribers')
+    price = IntegerField('Price', validators=[InputRequired()])
 
 
 @app.route('/')
@@ -89,7 +123,8 @@ def signup():
             flash("User already exists!")
             return redirect(url_for('signup'))
         hashed_password = generate_password_hash(form.password.data, method='sha256')
-        new_user = User(name=form.name.data, email=(form.email.data).lower(), password=hashed_password, type=form.type.data)
+        new_user = User(name=form.name.data, email=(form.email.data).lower(), password=hashed_password,
+                        type=form.type.data)
         db.session.add(new_user)
         db.session.commit()
 
@@ -97,6 +132,28 @@ def signup():
         return redirect(url_for('login'))
 
     return render_template('signup.html', form=form)
+
+
+@app.route('/add_marketplace', methods=['GET', 'POST'])
+@login_required
+def add_marketplace():
+    print(current_user.type)
+    if 'Brand/Agency' != current_user.type:
+        # flash('You can not access this page')
+        return redirect(url_for('marketplace'))
+    form = CreateChannelForm()
+    if form.validate_on_submit():
+        if Channel.query.filter_by(name=(form.name.data).lower()).first():
+            flash('Such marketplace already exists')
+            return redirect(url_for('marketplace'))
+        new_channel = Channel(name=form.name.data, description=form.description.data,
+                              subscribers=form.subscribers.data,
+                              price=form.price.data, category=form.category.data)
+        flash('Great! Your channel "%s" successfully added!' % new_channel.name)
+
+        db.session.add(new_channel)
+        db.session.commit()
+    return render_template('add_marketplace.html', form=form)
 
 
 @app.route('/marketplace')
@@ -126,10 +183,10 @@ def privacy():
 def contact():
     return render_template('contact.html')
 
+
 @app.route('/settings')
 @login_required
 def settings():
-
     return render_template('settings.html')
 
 
